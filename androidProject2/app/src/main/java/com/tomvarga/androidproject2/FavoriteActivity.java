886645cce29e34d439b78648470729d6972a6e99
tomvarga620 <tomas.varga@akademiasovy.sk>
2019-11-26 19:43:43 +0100

    added layout

--- androidProject2/app/src/main/java/com/tomvarga/androidproject2/FavoriteActivity.java
+++ androidProject2/app/src/main/java/com/tomvarga/androidproject2/FavoriteActivity.java
@@ -5,19 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class FavoriteActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_favorite);
 
         bottomNav = findViewById(R.id.bottom_navigation);
 
@@ -26,17 +25,17 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch(menuItem.getItemId()){
                     case R.id.home_action:
-                        startActivity(new Intent(MainActivity.this,MainActivity.class));
+                        startActivity(new Intent(FavoriteActivity.this,MainActivity.class));
                         finish();
                         break;
 
                     case R.id.favorite_action:
-                        Intent favoriteIntent = new Intent(MainActivity.this,FavoriteActivity.class);
-                        startActivity(favoriteIntent);
+                        startActivity(new Intent(FavoriteActivity.this,FavoriteActivity.class));
+                        finish();
                         break;
 
                     case R.id.settings_action:
-                        Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
+                        Intent settingsIntent = new Intent(FavoriteActivity.this, SettingsActivity.class);
                         startActivity(settingsIntent);
                         break;
                 }return true;
--- androidProject2/app/src/main/java/com/tomvarga/androidproject2/SettingsActivity.java
+++ androidProject2/app/src/main/java/com/tomvarga/androidproject2/SettingsActivity.java
@@ -5,19 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings);
 
         bottomNav = findViewById(R.id.bottom_navigation);
 
@@ -26,18 +25,18 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch(menuItem.getItemId()){
                     case R.id.home_action:
-                        startActivity(new Intent(MainActivity.this,MainActivity.class));
-                        finish();
+                        Intent homeIntent = new Intent(SettingsActivity.this,MainActivity.class);
+                        startActivity(homeIntent);
                         break;
 
                     case R.id.favorite_action:
-                        Intent favoriteIntent = new Intent(MainActivity.this,FavoriteActivity.class);
+                        Intent favoriteIntent = new Intent(SettingsActivity.this,FavoriteActivity.class);
                         startActivity(favoriteIntent);
                         break;
 
                     case R.id.settings_action:
-                        Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
-                        startActivity(settingsIntent);
+                        startActivity(new Intent(SettingsActivity.this,SettingsActivity.class));
+                        finish();
                         break;
                 }return true;
             }
