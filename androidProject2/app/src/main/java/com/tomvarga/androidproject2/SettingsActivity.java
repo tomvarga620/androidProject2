8549dc8e43c31248381d88e5c43ad099008c3754
tomvarga620 <tomas.varga@akademiasovy.sk>
2019-11-27 22:29:02 +0100

    added splash screen with logo

--- androidProject2/app/src/main/java/com/tomvarga/androidproject2/SettingsActivity.java
+++ androidProject2/app/src/main/java/com/tomvarga/androidproject2/SettingsActivity.java
@@ -18,6 +18,8 @@ public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
